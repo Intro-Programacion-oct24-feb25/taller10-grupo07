82eb2b5b42b3efd1e1a9873161f69a087d5c5b78
Dalton Flores <daltonflores@gamil.com>
2024-12-19 12:33:28 -0500

    segundo cambio

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,20 +10,45 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    String[][] notasCualitativas = new String[3][3];
-	double[][] notasCuantitativas1 = {{7.1, 8, 10}, {3, 7, 9}, {10, 9, 2.1}};
-        for(int fila = 0; fila < notasCuantitativas.length; fila++){
-            for(int col = 0; col < notasCuantitativas1[fila].length; col++) {
-                if (notasCuantitativas1 >0 && notasCuantitativas1 <3 )
-                    notasCuantitativas
-        /*
-        Referencia para obtener el valor cualitativo de una nota numérica
+
+    public static void main(String[] args) {
+            double[][] notasCuantitativas = {
+                {7.1, 8, 10},
+                {3, 7, 9},
+                {10, 9, 2.1}
+            };
+            String[][] notasCualitativas = new String[3][3];
+
+            /*
+        Rango de notas cualitativas:
         0 - 2.9   Insuficiente
         3 - 4.9   Regular
         5 - 7.9   Buena
         8 - 9.5   Muy Buena
-        9.6 - 10  Sobresaliente  
-        */
-         }
+        9.6 - 10  Sobresaliente
+             */
+            for (int i = 0; i < notasCuantitativas.length; i++) {
+                for (int j = 0; j < notasCuantitativas[i].length; j++) {
+                    double nota = notasCuantitativas[i][j];
+
+                    if (nota >= 0 && nota <= 2.9) {
+                        notasCualitativas[i][j] = "Insuficiente";
+                    } else if (nota >= 3 && nota <= 4.9) {
+                        notasCualitativas[i][j] = "Regular";
+                    } else if (nota >= 5 && nota <= 7.9) {
+                        notasCualitativas[i][j] = "Buena";
+                    } else if (nota >= 8 && nota <= 9.5) {
+                        notasCualitativas[i][j] = "Muy Buena";
+                    } else if (nota > 9.5 && nota <= 10) {
+                        notasCualitativas[i][j] = "Sobresaliente";
+                    } else {
+                        notasCualitativas[i][j] = "Nota inválida";
+                    }
+
+                    // Mostrar la nota y su condición cualitativa
+                    System.out.printf("Nota: %.1f - Condición: %s\n", nota, notasCualitativas[i][j]);
+                }
+            }
+        }
     }
-}
+
